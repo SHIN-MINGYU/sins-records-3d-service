aed0a7d336ab77b2a95c5dfc4b21b4cf742484c1
SHIN-MINGYU <smg20004@naver.com>
2023-07-03 22:13:12 +0900

    git cached deleted

---
+++ vue_front/src/App.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { ref, onMounted } from "@vue/runtime-core";
+import EnviromentsScene from "./utils/scenes/EnviromentsScene";
+import CharacterScene from "./utils/scenes/CharacterScene";
+
+export default {
+  name: "BabylonScene",
+  setup() {
+    const bjsCanvas = ref<HTMLCanvasElement | null>(null);
+    const bootStrap = async () => {
+      if (bjsCanvas.value) {
+        const scene = new EnviromentsScene(bjsCanvas.value);
+        scene.createBall();
+        scene.createGround();
+         new CharacterScene(scene);
+      }
+    }
+
+    onMounted(() => {
+      bootStrap()
+    });
+
+    return {
+      bjsCanvas,
+    };
+  },
+};
+</script>
+
+<template>
+  <canvas id="mainCanvas" ref="bjsCanvas" />
+</template>
+
+<style scoped>
+#mainCanvas{
+  height : 100vh;
+  width : 100vw;
+  display : block
+}
+</style>
